4a43f058d666a28e0cdc7399edf05076fedc2dc8
Alex Chan <alex@alexwlchan.net>
2017-05-12 22:08:49 +0100

    Push the package update

--- test_alexwlchan_foo/setup.py
+++ test_alexwlchan_foo/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='test_alexwlchan_foo',
-    version='0.0.1',
+    version='0.0.2',
     description='A test package',
     author='Alex Chan',
     author_email='alex@alexwlchan.net',
--- test_alexwlchan_foo/test_alexwlchan_foo.py
+++ test_alexwlchan_foo/test_alexwlchan_foo.py
@@ -2,3 +2,7 @@
 
 def hello(name):
     print('Hello ' + name)
+
+
+def howdy(name):
+    print('Howdy ' + name)
